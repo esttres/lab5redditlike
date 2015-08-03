5e4a22f3e7ce76e46c4f48ac7de2d926b8a8fddf
esttres <esttres@hotmail.com>
2015-08-03 08:49:52 -0400

    Seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create(:id => 1, :title => 'The first URL', :summary => 'this is the first URL',
+            :rl_url => 'www.yahoo.com', :user_id => 1, :vote_total => 0)
+
+Post.create(:id => 2, :title => 'The second URL', :summary => 'this is the second URL',
+            :rl_url => 'www.hotmail.com', :user_id => 2, :vote_total => 0)
