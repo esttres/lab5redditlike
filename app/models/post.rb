d9c3a9fbd524770b4b38154f98b046aec592d0c8
esttres <esttres@hotmail.com>
2015-08-04 07:14:39 -0400

    add uniquenees validation for rl_url

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
 
   validates_presence_of :title, :summary, :rl_url, :user_id
+  validates_uniqueness_of :rl_url
   has_many :votes, dependent: :destroy
   has_many :dvotes, dependent: :destroy
 
