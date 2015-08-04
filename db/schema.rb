e2a3766d7d86a1016b84d804dec22e16dfe6686e
esttres <esttres@hotmail.com>
2015-08-04 15:36:51 -0400

    Several changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150803174357) do
+ActiveRecord::Schema.define(version: 20150804173248) do
 
   create_table "dvotes", force: :cascade do |t|
     t.integer  "post_id"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20150803174357) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
