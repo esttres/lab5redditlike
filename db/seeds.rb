a7279603adcaf944b2a03c6daa9925b1b66de37c
esttres <esttres@hotmail.com>
2015-08-05 10:15:48 -0400

    changed seed to add user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Post.create(:id => 3, :title => 'The first seed URL', :summary => 'this is the first seed URL',
+Post.create(:id => 1, :title => 'The first seed URL', :summary => 'this is the first seed URL',
             :rl_url => 'www.yahoo.com', :user_id => 1, :vote_total => 0)
 
-Post.create(:id => 4, :title => 'The second seed URL', :summary => 'this is the second seed URL',
-            :rl_url => 'www.hotmail.com', :user_id => 2, :vote_total => 0)
+Post.create(:id => 2, :title => 'The second seed URL', :summary => 'this is the second seed URL',
+            :rl_url => 'www.hotmail.com', :user_id => 1, :vote_total => 0)
+
+User.create!(:name => 'Silas Tolliver', :email => 'esttres@hotmail.com', :password => '123456789',
+             :password_confirmation => '123456789')
